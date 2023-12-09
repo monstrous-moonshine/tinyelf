eb14d7f3d6496aa39974342c4debe4087d79fd98
Khalid Bin Walid <kbwalid26@gmail.com>
2023-12-10 00:11:04 +0900

    add comments, pull out write into separate header

--- hello.c
+++ hello.c
@@ -1,31 +1,19 @@
-#include <sys/syscall.h>
-#include <sys/types.h>
+#include "util.h"
 
 #define STDOUT_FILENO 1
 #define _tdata_ __attribute__((section(".text#"), aligned(1)))
 
-static inline ssize_t write(int fd, const void *buf, size_t count) {
-    ssize_t ret;
-    asm volatile (
-        "syscall"
-        : "=a"(ret)
-        : "a"(__NR_write), "D"(fd), "S"(buf), "d"(count)
-        : "rcx", "r11", "memory"
-    );
-    return ret;
-}
-
-#if 0
-static size_t strlen(const char *s) {
-    size_t cnt = 0;
-    while (*s++) cnt++;
-    return cnt;
-}
-#endif
 
 int main(int argc, char *argv[]) {
+    // We keep the @argc and the @argv arguments to remind ourselves
+    // that our startup routine calls main with these arguments and
+    // we could use them if we wanted. Here, we don't.
     (void)argc;
     (void)argv;
+    // Keep it in the .text section along with the code. Look at the
+    // generated assembly (make hello.S) to understand the # at the
+    // end.
     static _tdata_ const char message[] = "Hello, world!\n";
+    // We don't need to write the terminating NULL, so subtract 1.
     write(STDOUT_FILENO, message, sizeof message - 1);
 }
